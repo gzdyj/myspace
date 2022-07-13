853ebede86a52fefb7c2a941e55db9c9ae421d44
gzdyj <86736734@qq.com>
2022-07-13 19:28:33 +0800

    v1.01

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,17 +1,53 @@
 <template>
     <Content>
-        用户列表
+        <div class="card" v-for="user in users" :key="user.id">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-1">
+                <img class="img-fluid" :src="user.photo" alt="">
+              </div>
+              <div class="col-11">
+                <div class = "username">{{ user.username }}</div>
+                
+                <div class = "follower-count">{{ user.followerCount }}</div>
+
+              </div>
+            </div>
+          </div>
+        </div>
     </Content>
 </template>
 
 <script>
 
-import Content from '../components/Content.vue'
+//# sourceMappingURL=file.js.map
+
+import Content from '../components/Content.vue';
+import $ from 'jquery';
+import { ref } from 'vue';
+
 
 export default {
   name: 'userList',
   components: {
     Content,
+  },
+
+  setup() {
+    let users = ref([]);
+
+    $.ajax({
+      url: 'https://app165.acapp.acwing.com.cn/myspace/userlist/',
+      type: "get",
+      success(resp) {
+        users.value = resp;
+      }
+
+    });
+
+    return {
+      users,
+    };
   }
 }
 </script>
@@ -21,5 +57,31 @@ export default {
   margin-top: 20px;
   text-align: left;
 }
+
+img {
+  border-radius: 50%;
+}
+
+.username {
+  font-weight: bold;
+  height: 50%;
+}
+
+.follower-count {
+  font-size: 12px;
+  color: gray;
+  height: 50%;
+}
+
+.card{
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+
+.card:hover {
+  box-shadow: 2px 2px 10px lightpink;
+  transition: 500ms;
+}
+
 
 </style>
